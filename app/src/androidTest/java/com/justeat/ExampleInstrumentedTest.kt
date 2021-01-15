ea455547d561d114c4b00e099f168c6e4bcdfdde
Juma Allan <allanjuma@gmail.com>
2021-01-15 19:17:26 +0300

    added Fastlane

--- app/src/androidTest/java/com/justeat/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/justeat/ExampleInstrumentedTest.kt
@@ -32,6 +32,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ke.co.appslab.gradleplugins", appContext.packageName)
+        assertEquals("com.justeat", appContext.packageName)
     }
 }
