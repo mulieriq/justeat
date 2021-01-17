eaeb0858dd6f2784c04947cab712ee65e3c2204d
Juma Allan <allanjuma@gmail.com>
2021-01-17 16:00:57 +0300

    chore: setting up chips, and demo restaurants

--- presentation/src/main/java/com/justeat/presentation/ui/view/MainActivity.kt
+++ presentation/src/main/java/com/justeat/presentation/ui/view/MainActivity.kt
@@ -17,9 +17,11 @@ package com.justeat.presentation.ui.view
 
 import android.os.Bundle
 import com.justeat.presentation.R
+import com.justeat.presentation.data.Restaurant
 import com.justeat.presentation.databinding.ActivityMainBinding
 import com.justeat.presentation.ui.adapter.RestaurantRecyclerViewAdapter
 import com.justeat.presentation.ui.base.BindingActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BindingActivity<ActivityMainBinding>() {
 
@@ -27,9 +29,23 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         binding.lifecycleOwner = this
+
+        restaurantRecyclerViewAdapter = RestaurantRecyclerViewAdapter()
+        recyclerView.adapter = restaurantRecyclerViewAdapter
+
+        restaurantRecyclerViewAdapter.submitList(prepareDemoRestaurants())
     }
 
     override val layoutResId: Int
         get() = R.layout.activity_main
+
+    private fun prepareDemoRestaurants(): List<Restaurant> {
+        val restaurants = ArrayList<Restaurant>()
+        restaurants.add(Restaurant("Tanoshii Sushi", "open", 4.5, 1190, 1000, 200))
+        restaurants.add(Restaurant("Tandoori Express", "closed", 4.5, 2308, 1300, 150))
+        restaurants.add(Restaurant("Royal Thai", "order_ahead", 4.5, 2639, 2500, 150))
+        return restaurants
+    }
 }
