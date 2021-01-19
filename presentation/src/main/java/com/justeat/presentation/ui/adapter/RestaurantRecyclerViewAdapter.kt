9b6c1101f187557e80e07cad71052f8bcd6a1252
Juma Allan <allanjuma@gmail.com>
2021-01-19 19:15:32 +0300

    cleaning up the UIs - setting up favourite

--- presentation/src/main/java/com/justeat/presentation/ui/adapter/RestaurantRecyclerViewAdapter.kt
+++ presentation/src/main/java/com/justeat/presentation/ui/adapter/RestaurantRecyclerViewAdapter.kt
@@ -23,6 +23,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.justeat.presentation.data.Restaurant
 import com.justeat.presentation.databinding.RowRestaurantBinding
 
+typealias OnFavouriteClickListener = (Restaurant) -> Unit
+
 object RestaurantDiffer : DiffUtil.ItemCallback<Restaurant>() {
 
     override fun areItemsTheSame(oldItem: Restaurant, newItem: Restaurant) =
@@ -31,19 +33,30 @@ object RestaurantDiffer : DiffUtil.ItemCallback<Restaurant>() {
     override fun areContentsTheSame(oldItem: Restaurant, newItem: Restaurant) = oldItem == newItem
 }
 
-internal class RestaurantRecyclerViewAdapter :
+internal class RestaurantRecyclerViewAdapter(
+    private val onFavouriteClickListener: OnFavouriteClickListener
+) :
     ListAdapter<Restaurant, RestaurantRecyclerViewAdapter.ViewHolder>(RestaurantDiffer) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(RowRestaurantBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        ViewHolder(
+            RowRestaurantBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            onFavouriteClickListener
+        )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
         holder.bind(getItem(position))
 
-    inner class ViewHolder(private val binding: RowRestaurantBinding) :
+    inner class ViewHolder(
+        private val binding: RowRestaurantBinding,
+        private val onFavouriteClickListener: OnFavouriteClickListener
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(restaurant: Restaurant) {
             binding.restaurant = restaurant
+            binding.btnFavourite.setOnClickListener {
+                onFavouriteClickListener(restaurant)
+            }
             binding.executePendingBindings()
         }
     }
