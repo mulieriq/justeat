986b98f0e8415117db1e3474828ae083fa65c529
Juma Allan <allanjuma@gmail.com>
2021-01-19 16:35:46 +0300

    chore: cleaning up UI

--- presentation/src/main/java/com/justeat/presentation/ui/view/MainActivity.kt
+++ presentation/src/main/java/com/justeat/presentation/ui/view/MainActivity.kt
@@ -17,35 +17,30 @@ package com.justeat.presentation.ui.view
 
 import android.os.Bundle
 import com.justeat.presentation.R
-import com.justeat.presentation.data.Restaurant
 import com.justeat.presentation.databinding.ActivityMainBinding
 import com.justeat.presentation.ui.adapter.RestaurantRecyclerViewAdapter
 import com.justeat.presentation.ui.base.BindingActivity
+import com.justeat.presentation.ui.viewmodel.RestaurantsViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BindingActivity<ActivityMainBinding>() {
 
     private lateinit var restaurantRecyclerViewAdapter: RestaurantRecyclerViewAdapter
+    private val restaurantsViewModel: RestaurantsViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding.lifecycleOwner = this
 
         restaurantRecyclerViewAdapter = RestaurantRecyclerViewAdapter()
         recyclerView.adapter = restaurantRecyclerViewAdapter
 
-        restaurantRecyclerViewAdapter.submitList(prepareDemoRestaurants())
+        restaurantsViewModel.restaurants.observe(this) { restaurants ->
+            restaurantRecyclerViewAdapter.submitList(restaurants)
+        }
     }
 
     override val layoutResId: Int
         get() = R.layout.activity_main
-
-    private fun prepareDemoRestaurants(): List<Restaurant> {
-        val restaurants = ArrayList<Restaurant>()
-        restaurants.add(Restaurant("Tanoshii Sushi", "open", 4.5, 1190, 1000, 200))
-        restaurants.add(Restaurant("Tandoori Express", "closed", 4.5, 2308, 1300, 150))
-        restaurants.add(Restaurant("Royal Thai", "order_ahead", 4.5, 2639, 2500, 150))
-        return restaurants
-    }
 }
