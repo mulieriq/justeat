1832856ecea8fe5f0cc2bddbed11a91115728c9d
Juma Allan <allanjuma@gmail.com>
2021-01-20 20:52:31 +0300

    chore: cleaning up presentation ui layer

--- presentation/src/main/java/com/justeat/presentation/ui/view/MainActivity.kt
+++ presentation/src/main/java/com/justeat/presentation/ui/view/MainActivity.kt
@@ -16,13 +16,16 @@
 package com.justeat.presentation.ui.view
 
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
+import com.google.android.material.chip.Chip
+import com.justeat.core.extensions.hide
+import com.justeat.core.extensions.show
 import com.justeat.presentation.R
 import com.justeat.presentation.data.Restaurant
 import com.justeat.presentation.databinding.ActivityMainBinding
 import com.justeat.presentation.ui.adapter.RestaurantRecyclerViewAdapter
 import com.justeat.presentation.ui.base.BindingActivity
 import com.justeat.presentation.ui.viewmodel.RestaurantsViewModel
-import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BindingActivity<ActivityMainBinding>() {
@@ -37,18 +40,78 @@ class MainActivity : BindingActivity<ActivityMainBinding>() {
         restaurantRecyclerViewAdapter = RestaurantRecyclerViewAdapter { restaurant ->
             onFavourite(restaurant)
         }
-        recyclerView.adapter = restaurantRecyclerViewAdapter
 
-        restaurantsViewModel.fetchRestaurants("", "")
+        binding.recyclerView.itemAnimator = null
+        binding.recyclerView.adapter = restaurantRecyclerViewAdapter
 
-        restaurantsViewModel.restaurants.observe(this) { restaurants ->
-            restaurantRecyclerViewAdapter.submitList(restaurants)
-        }
+        setUpRestaurantsList()
+        setUpSearch()
+        setUpFilters()
+    }
+
+    private fun setUpRestaurantsList() {
+        restaurantsViewModel.fetchRestaurants()
+        observeData()
     }
 
     private fun onFavourite(restaurant: Restaurant) {
         val updateRestaurant = restaurant.copy(isFavourite = true)
         restaurantsViewModel.favouriteRestaurant(updateRestaurant)
+    }
+
+    private fun setUpSearch() {
+        binding.toolbar.searchInput.doAfterTextChanged { searchString ->
+            if (!searchString.isNullOrEmpty()) {
+                restaurantsViewModel.searchRestaurants(searchString = searchString.toString())
+            } else {
+                setUpRestaurantsList()
+            }
+        }
+    }
+
+    private fun setUpFilters() {
+        binding.toolbar.chipGroup.setOnCheckedChangeListener { _, checkedId ->
+            val selectedChip = binding.toolbar.chipGroup.findViewById<Chip>(checkedId)
+            if (selectedChip?.isChecked == true) {
+                val sortBy = selectedChip.text.toString().returnSortByFilter()
+                restaurantsViewModel.filterRestaurants(sortBy = sortBy)
+            } else {
+                observeData()
+            }
+        }
+    }
+
+    private fun observeData() {
+        restaurantsViewModel.restaurants.observe(this) { restaurants ->
+            if (!restaurants.isNullOrEmpty()) {
+                showListState()
+                restaurantRecyclerViewAdapter.submitList(restaurants)
+            } else {
+                showEmptyState()
+            }
+        }
+    }
+
+    private fun showListState() {
+        binding.swipeRefreshLayout.show()
+        binding.layoutEmpty.hide()
+    }
+
+    private fun showEmptyState() {
+        binding.swipeRefreshLayout.hide()
+        binding.layoutEmpty.show()
+    }
+
+    private fun String.returnSortByFilter(): String? = when (this) {
+        resources.getString(R.string.chip_best_match) -> "bestMatch"
+        resources.getString(R.string.chip_newest) -> "newest"
+        resources.getString(R.string.chip_rating_average) -> "ratingAverage"
+        resources.getString(R.string.chip_distance) -> "distance"
+        resources.getString(R.string.chip_popularity) -> "popularity"
+        resources.getString(R.string.chip_average_product_price) -> "averageProductPrice"
+        resources.getString(R.string.chip_delivery_cost) -> "deliveryCosts"
+        resources.getString(R.string.chip_minimum_cost) -> "minCost"
+        else -> null
     }
 
     override val layoutResId: Int
